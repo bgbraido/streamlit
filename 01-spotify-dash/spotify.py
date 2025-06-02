8e00b424cc7191f87174b1a64e95b60ec70ea2c1
Bruno Braido <Bruno.Braido@dsm.com>
2025-06-02 18:17:59 +0200

    feat(streamlit): Added charts

--- 01-spotify-dash/spotify.py
+++ 01-spotify-dash/spotify.py
@@ -2,4 +2,7 @@ import streamlit as st
 import pandas as pd
 
 df = pd.read_csv("01-spotify-dash/01 Spotify.csv")
+
+df.set_index("Artist", inplace=True)
+st.line_chart(df[df["Stream"] > 1000000000]["Stream"])
 df

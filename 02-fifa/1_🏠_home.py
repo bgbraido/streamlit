0bcfa953462e2c24170346faf6468349dbfb55fc
Bruno Braido <Bruno.Braido@dsm.com>
2025-06-03 12:13:44 +0200

    feat(fifa): Load players page

--- 02-fifa/1_🏠_home.py
+++ 02-fifa/1_🏠_home.py
@@ -1,1 +1,35 @@
 import streamlit as st
+import webbrowser
+import pandas as pd
+from datetime import datetime
+
+if "data" not in st.session_state:
+    df_data = pd.read_csv("02-fifa/datasets/CLEAN_FIFA23_official_data.csv", index_col=0)
+    df_data = df_data[df_data["Contract Valid Until"] >= datetime.today().year]
+    df_data = df_data[df_data["Value(£)"] > 0]
+    df_data = df_data.sort_values(by="Overall", ascending=False)
+    st.session_state["data"] = df_data
+
+st.markdown("# FIFA 2023 Offical Dataset ⚽️")
+st.sidebar.markdown("Developed by B. Giannelli Braido 🇧🇷🇮🇹")
+
+btn = st.button("Go to Kaggle data")
+if btn:
+    webbrowser.open_new_tab("https://www.kaggle.com/datasets/kevwesophia/fifa23-official-datasetclean-data")
+
+st.markdown(
+    """
+    O conjunto de dados
+    de jogadores de futebol de 2017 a 2023 fornece informações 
+    abrangentes sobre jogadores de futebol profissionais.
+    O conjunto de dados contém uma ampla gama de atributos, incluindo dados demográficos 
+    do jogador, características físicas, estatísticas de jogo, detalhes do contrato e 
+    afiliações de clubes. 
+    
+    Com **mais de 17.000 registros**, este conjunto de dados oferece um recurso valioso para 
+    analistas de futebol, pesquisadores e entusiastas interessados em explorar vários 
+    aspectos do mundo do futebol, pois permite estudar atributos de jogadores, métricas de 
+    desempenho, avaliação de mercado, análise de clubes, posicionamento de jogadores e 
+    desenvolvimento do jogador ao longo do tempo.
+"""
+)
---
+++ 02-fifa/pages/2_🏃🏽‍♂️_players.py
@@ -0,0 +1,5 @@
+import streamlit as st
+
+
+df_data = st.session_state["data"]
+df_data
